763f58b2e0c9161412835b7570c68e0308bd61d5
mjhct <mjhct0358@163.com>
2021-06-15 13:45:21 +0800

    享元模式

---
+++ code/pattern/src/main/java/mjhct/designpattern/pattern/flyweight/QiZi.java
@@ -0,0 +1,30 @@
+package mjhct.designpattern.pattern.flyweight;
+
+/**
+ * 围棋棋子
+ */
+public abstract class QiZi {
+
+    private boolean use;
+
+    /**
+     * 落子，use要置为true
+     */
+    abstract void luoZi(int x, int y);
+
+    public boolean isUse() {
+        return use;
+    }
+
+    public void setUse(boolean use) {
+        this.use = use;
+    }
+
+    /**
+     * 被围住吃了，use要置为false
+     */
+    public void beEaten() {
+        use = false;
+    }
+
+}
---
+++ code/pattern/src/main/java/mjhct/designpattern/pattern/flyweight/QiZiFactory.java
@@ -0,0 +1,45 @@
+package mjhct.designpattern.pattern.flyweight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 围棋棋子工厂
+ */
+public class QiZiFactory {
+
+    private static List<BlackQiZi> blackQiZiList = new ArrayList<>(16);
+
+    private static List<WhiteQiZi> whiteQiZiList = new ArrayList<>(16);
+
+    static {
+        // 假设棋盘最多容纳10个棋子，那么黑白棋子最多10个
+        for (int i=0; i<10; i++) {
+            blackQiZiList.add(new BlackQiZi());
+            whiteQiZiList.add(new WhiteQiZi());
+        }
+    }
+
+    BlackQiZi getHeiZi() {
+        for (BlackQiZi blackQiZi : blackQiZiList) {
+            if (!blackQiZi.isUse()) {
+                return blackQiZi;
+            }
+        }
+        // 在这里这是不可能发生的事情，抛出异常
+        System.out.println("黑子用完，不可能");
+        return null;
+    }
+
+    WhiteQiZi getBaiZi() {
+        for (WhiteQiZi whiteQiZi : whiteQiZiList) {
+            if (!whiteQiZi.isUse()) {
+                return whiteQiZi;
+            }
+        }
+        // 在这里这是不可能发生的事情，抛出异常
+        System.out.println("白子用完，不可能");
+        return null;
+    }
+
+}
